2e797dce74ac7f41179699f49b8aa2180e6030b3
1gee1 <1gee1@naver.com>
2017-09-18 14:36:26 +0900

    gdgd

---
+++ JAVA/Work/JavaApplicaption/src/main/java/java02/jv02_24_형변환.java
@@ -0,0 +1,16 @@
+package java02;
+
+
+public class jv02_24_형변환 {
+    public static void main(String[] args) {
+        
+        int result = 1 + '2' ;
+        System.out.println(result);
+        
+       // result = '2' + (b)true ;
+       // System.out.println(result);
+        
+      result = Integer.parseInt("15") - 5 ;
+        System.out.println(result);
+    }
+}
