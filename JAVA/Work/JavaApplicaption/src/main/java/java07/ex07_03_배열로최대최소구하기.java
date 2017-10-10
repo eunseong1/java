f2bc2ee877ea49be80a166b72c98ab2b3ea64269
1gee1 <1gee1@naver.com>
2017-10-10 09:50:24 +0900

    sds

--- JAVA/Work/JavaApplicaption/src/main/java/java07/ex07_03_배열로최대최소구하기.java
+++ JAVA/Work/JavaApplicaption/src/main/java/java07/ex07_03_배열로최대최소구하기.java
@@ -1,15 +1,34 @@
 package java07;
 
+import java.util.Arrays;
+
 public class ex07_03_배열로최대최소구하기 {
     public static void main(String[] args) {
         
-        int[] x = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        System.out.print("배열 정렬 전 :");
+        int[] arr = { 1, 3, 4, 5, 7, 6, 56, 34, 54, 23 };
         
-        System.out.print("INPUT : ");
-        for (int i = 0; i <= x.length - 1; i++) {
-            
-            System.out.print(x[i] + " ");
+        for (int i = 0; i <= arr.length - 1; i++) {
+            System.out.print(arr[i]);//배열 값넣기
+            if (i == arr.length - 1) {
+                System.out.println(".");
+            } else {
+                System.out.print(",");
+            }
         }
+        System.out.print("배열 정렬 후 :");
+        for (int i = 0; i <= arr.length-1; i++) {
+            Arrays.sort(arr);
+            System.out.print(arr[i]);
+            if (i == arr.length - 1) {
+                System.out.println(".");
+            } else {
+                System.out.print(",");
+            }
+            
+        }
+        System.out.println("최소값: " + arr[0]);//정렬되서 최소값은 첫째
+        System.out.print("최대값: " + arr[arr.length - 1]);
     }
     
 }
